36ea42c5290c47d34feb0b97d188980f678b7ce6
renan martins <renanmartins989@gmail.com>
2023-05-22 05:00:26 +0100

    exemplo feito, falta apenas a responsividade que vou finalizar mais tarde

--- vue04-techno-project/app.js
+++ vue04-techno-project/app.js
@@ -5,7 +5,10 @@ const vm = new Vue({
   data: {
     produtos: [],
     produto: false,
-    carrinho: []
+    carrinho: [],
+    mensagemAlerta: 'Item adicionado',
+    alertaAtivo: false,
+    carrinhoAtivo: false
   },
   filters: {
     numeroPreco(valor) {
@@ -46,16 +49,65 @@ const vm = new Vue({
     fecharModal({ target, currentTarget }) {
       if (target === currentTarget) this.produto = false
     },
+    clickForaCarrinho({ target, currentTarget }) {
+      if (target === currentTarget) this.carrinhoAtivo = false
+    },
     adicionarItem() {
         this.produto.estoque--
         const { id, nome,  preco } = this.produto
         this.carrinho.push({id, nome, preco})
+        this.alerta(`${nome} adicionado`)
     },
     removerItem(index) {
       this.carrinho.splice(index, 1)
+      this.alerta('Removido do carrinho')
+    },
+    checarLocalStorage() {
+      if (window.localStorage.carrinho) {
+        // returnando o valor em string alterado, para array e salvando em carrinho
+        this.carrinho = JSON.parse(window.localStorage.carrinho)
+      }
+    },
+    alerta(mensagem) {
+      this.mensagemAlerta = mensagem
+      this.alertaAtivo = true
+      setTimeout(() => {
+        this.alertaAtivo = false
+      }, 1500 );
+    },
+    compararEstoque() {
+      const items = this.carrinho.filter(({ id }) => {
+        if (id === this.produto.id){
+          return true
+        }
+      })
+      this.produto.estoque -= items.length
+    },
+    router() {
+      const hash = document.location.hash
+      if (hash) {
+        this.fetchProduto(hash.replace('#', ''))
+      }
+    }
+  },
+  watch: {
+    // Routes
+    produto() {
+      document.title = this.produto.nome || "Techno"
+      const hash = this.produto.id || ""
+      history.pushState(null, null, `#${hash}`)
+      if (this.produto) {
+        this.compararEstoque()
+      }
+    },
+    carrinho() {
+      // transformando em string para salvar em localStorage o valor de carrinho
+      window.localStorage.carrinho =JSON.stringify(this.carrinho)
     }
   },
   created() {
     this.fetchProdutos()
+    this.router()
+    this.checarLocalStorage()
   }
 })
