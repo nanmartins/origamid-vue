005f2d1e1795742cf74d6153e927675de630235f
renan martins <renanmartins989@gmail.com>
2023-06-01 00:10:04 +0100

    components slots

---
+++ vue05-components/vue-slots/src/App.vue
@@ -0,0 +1,38 @@
+
+<template>
+  <div id="app">
+    <modal-principal>
+
+      <template v-slot:header="logo">
+        {{ logo.logo }}
+        Esse é o header
+      </template>
+
+      <template v-slot:default="{ user }">
+        <!-- {{ user.user.nome }} -->
+        <p>{{ user.nome }}</p>
+        <p>Esse é o paragrafo dentro do slot sem nome</p>
+        <button>Comprar</button>
+      </template>
+
+      <template v-slot:footer>
+        <p>Esse é o footer</p>
+      </template>
+    </modal-principal>
+  </div>
+</template>
+
+<script>
+import ModalPrincipal from './components/ModalPrincipal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ModalPrincipal
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue05-components/vue-slots/src/components/ModalPrincipal.vue
@@ -0,0 +1,28 @@
+
+
+<template>
+  <div class="modal">
+    <slot name="header" :logo="logo"></slot>
+    <h1>{{ user }}</h1>
+    <slot :user="user"></slot>
+    <slot name="footer"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ModalPrincipal',
+  data() {
+    return {
+      logo: "Techno",
+      user: {
+        nome: "Renan"
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
