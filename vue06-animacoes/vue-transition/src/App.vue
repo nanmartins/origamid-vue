c4cb29582b0f2613ed8668ed87d6072c4761cfd4
renan martins <renanmartins989@gmail.com>
2023-06-02 14:48:22 +0100

    vue animations lesson 1 done

---
+++ vue06-animacoes/vue-transition/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <button @click="ativo = !ativo">Mostrar</button>
+    <transition appear name="fade" enter-active-class="animar" @enter="enter" @leave="leave">
+      <!-- <h1 v-if="ativo">Servicos</h1> -->
+      <!-- usando appear, a animacao so ocorre quando o elemento estiver visivel -->
+      <h1>Servicos</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      ativo: false
+    }
+  },
+  methods: {
+    enter(elemento) {
+      console.log('Elemento entrou', elemento)
+    },
+    leave(elemento) {
+      console.log('Elementou saiu', elemento)
+    }
+  }
+}
+</script>
+
+<style>
+
+/* .fade-enter-active, .fade-leave-active {
+  transition: opacity 2s;
+} */
+
+.animar, .fade-leave-active {
+  transition: opacity 2s;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+
+</style>
